a6e2a48ee5543aeef78a8e2b76948b21ef26d217
Srivatsan <vatsan1993@gmail.com>
2023-09-28 21:53:08 +0530

    finished 4th program

--- 1-base-conversions/4-asciiTo8bit.js
+++ 1-base-conversions/4-asciiTo8bit.js
@@ -5,9 +5,24 @@ const addZeros = require('../utils/addZeros');
 //    Read the export file for the explanation of how it works
 
 /******************************************************************************/
-
-const asciiTo8bit = str => {
+const numToBin = (element) => {
+  let str = '';
+  while (element !== 0) {
+    str = (element % 2) + str;
+    element = parseInt(element / 2);
+  }
+  return str;
+};
+const asciiTo8bit = (str) => {
   // Your code here
+  let result = '';
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
+    let binary = numToBin(charCode);
+    binary = addZeros(binary, 8);
+    result += binary;
+  }
+  return result;
 };
 
 /******************************************************************************/
